ce34814c353f0bcc0c33fe583cff9a56b04d0921
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-08-01 09:45:31 +0200

    Fixes #87 nsp deprecation (patch)

--- test/modules-test.js
+++ test/modules-test.js
@@ -2,7 +2,7 @@ const test = require('ava')
 const pkg = require('../package.json')
 const dependencies = pkg.dependencies || {}
 const devDependencies = pkg.devDependencies || {}
-const dropModules = ['nsp']
+const dropModules = []
 const isDropped = module => !dropModules.includes(module)
 
 test('basic check', t => {
